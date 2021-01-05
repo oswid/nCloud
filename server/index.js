c1ff06151b083a872f5cb5ab5ca56f32f9e2cbd2
oswid <maikado4u@gmail.com>
2021-01-05 08:31:17 +0300

    add db

--- server/index.js
+++ server/index.js
@@ -1,13 +1,22 @@
 const express = require("express")
 const mongoose = require("mongoose")
-const port = 5000
+const config = require("config")
+
+
+
+const PORT = config.get("PORT")
 
 const app = express()
 
-const start = () =>{
+
+const start = async () =>{
   try{
-    app.listen(port, ()=>{
-      console.log("server started on port:  ", port)
+    await mongoose.connect(config.get("dbUrl"),
+                           {useNewUrlParser: true,
+                           useUnifiedTopology: true})
+    
+    app.listen(PORT, ()=>{
+      console.log("server started on port:  ", PORT)
     })    
   }catch(e){
     console.log(e)
