be24cef52f537eaafe38184f89116ed61d2b227e
oswid <maikado4u@gmail.com>
2021-01-08 17:55:02 +0300

    add login to server

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -22,7 +22,7 @@ async (req, res)=>{
     if(candidate){
       return res.status(400).json({message: "this user already exist"})
     }
-    const hashPassword = await bcrypt.hash(password, 15)
+    const hashPassword = await bcrypt.hash(password, 4)
     const user = new User({email,password:hashPassword})
     await user.save()
     return res.json({message: "user was created"})
@@ -33,5 +33,36 @@ async (req, res)=>{
   }
 })
 
+router.post("/login",
+            [
+              check("email", "uncorrect e-mail").isEmail(),
+              check("password", "password must be longer then 4").isLength({min:4})
+            ],
+async (req, res)=>{
+  try{
+    const errors = validationResult(req)
+    if (!errors.isEmpty()){
+      res.status(400).json({message: "uncorrect request "})
+    }
+    const {email, password} = req.body
+
+    const user = await User.findOne({email})
+    const isPassValid = bcrypt.compareSync(password, user.password)
+    if (!isPassValid){
+      return res.status(400).json({message: "invalid password"})
+    }
+    
+    return res.json({
+      id: user.id,
+      email: email,
+      diskSpace: user.diskSpace,
+      avatar: user.avatar
+    })
+   
+  }catch(e){
+    console.log(e)
+    res.send({message:"server error"})
+  }
+})
 module.exports = router
 
