e125c59f76f1c5365c781700eb4e62b2aedf0ef0
root <root@DESKTOP-QE3UETL.localdomain>
2021-01-08 15:35:06 +0300

    add validation

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,10 +1,20 @@
 const Router = require("express")
 const User = require("./../models/User")
 const router =  new Router()
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcryptjs")
+const {check, validationResult} = require("express-validator")
 
-router.post("/reg",async (req, res)=>{
+router.post("/reg",
+            [
+              check("email", "uncorrect e-mail").isEmail(),
+              check("password", "password must be longer then 4").isLength({min:4})
+            ],
+async (req, res)=>{
   try{
+    const errors = validationResult(req)
+    if (!errors.isEmpty()){
+      res.status(400).json({message: "uncorrect request "})
+    }
     const {email, password} = req.body
 
     const candidate = await User.findOne({email})
@@ -12,8 +22,8 @@ router.post("/reg",async (req, res)=>{
     if(candidate){
       return res.status(400).json({message: "this user already exist"})
     }
-    const hashPassword = await bcrypt.hash(password, 5)
-    const user = new User({email, hashPassword})
+    const hashPassword = await bcrypt.hash(password, 15)
+    const user = new User({email,password:hashPassword})
     await user.save()
     return res.json({message: "user was created"})
    
@@ -24,3 +34,4 @@ router.post("/reg",async (req, res)=>{
 })
 
 module.exports = router
+
