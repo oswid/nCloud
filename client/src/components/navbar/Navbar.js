4ce9924593fbc9aaed5c3d4d331ceb126c9e5c63
oswid <maikado4u@gmail.com>
2021-01-12 22:52:41 +0300

    add navbar

---
+++ client/src/components/navbar/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="image">
+                <img src="" alt=""/>
+            </div>
+            <div className="menu">
+                <ul>
+                    <l>Sign in </l>
+                    <l>Registration</l>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
