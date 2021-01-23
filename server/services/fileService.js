b696d67043ded759486b6b3610ae73a1a784015a
oswid <maikado4u@gmail.com>
2021-01-23 23:07:21 +0300

    add dir creation

---
+++ server/services/fileService.js
@@ -0,0 +1,24 @@
+const fs = require("fs")
+
+class FileService{
+
+    createDir(file){
+        const filePath = `${config.get("filePath")}\\${file.user}\\${file.path}`
+        return new Promise((resolve, reject) => {
+            try{
+                if (!fs.existsSync(filePath)){
+                    fs.mkdirSync(filePath)
+                    return resolve({message: "File wal created"})
+                }else{
+                    return reject({message: "Name of file already exist"})
+                }
+            }catch{
+                return reject({message: "File error"})
+            }
+        })
+    }
+
+}
+
+
+module.exports = new FileService()
