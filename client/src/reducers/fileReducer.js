4f79b9d94a45e6fcf370fe03358e600e96aee6fb
oswid <maikado4u@gmail.com>
2021-01-31 23:46:48 +0300

    fileReducer

--- client/src/reducers/fileReducer.js
+++ client/src/reducers/fileReducer.js
@@ -1,11 +1,19 @@
+const SET_CURRENT_DIR = "SET_CURRENT_DIR"
+const SET_FILES = "SET_FILES"
 
 const defaultState = {
+    files: [],
+    currentDir: null
 }
 
 export default function fileReducer(state = defaultState, action) {
     switch (action.type) {
+        case SET_FILES: return {... state, files: action.payload}
+        case SET_CURRENT_DIR: return {...state, currentDir: action.payload}
         default:
             return state
     }
 }
 
+export const setFiles = (files)=>({action: SET_FILES, payload: files})
+export const setDir = (dir)=>({action: SET_CURRENT_DIR, payload: dir})
